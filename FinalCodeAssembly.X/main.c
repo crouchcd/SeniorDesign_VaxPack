ba35933bf0f8b21d70ea54258067472e0f8beb18
crouchcd <cameroncrouch36@gmail.com>
2018-04-14 18:18:02 -0500

    Changing acceptable temp range to include up to 45 degrees
    comments prepped for final addition to the program

--- FinalCodeAssembly.X/main.c
+++ FinalCodeAssembly.X/main.c
@@ -1,7 +1,7 @@
 #include "mcc_generated_files/mcc.h"
 #define FCY 4000000UL //FCY = FOSC / 2 unsigned long (UL) 
 /* PC24FJ128GA204 = 8MHz internal oscillator
- * must be defined before importing <libpic30.h> */ 
+ * must be defined before importing <libpic30.h> */
 #include <libpic30.h>
 #include "LCD.h"
 #include "xc.h"
@@ -52,7 +52,7 @@ int main(void) {
         ADCvalue = ADC1_GetConversion(TEMPERATURE_CHANNEL);
         Vout = ADCvalue * (tempRef / 4095);
         /*Vout is originally in centigrade, 50 is subtracted to
-         * get the reading as its centigrade value, then convert to F */ 
+         * get the reading as its centigrade value, then convert to F */
         Vout = ((Vout / 0.01) - 50) * 1.8 + 32;
         actualTemp = Vout;
 
@@ -61,24 +61,43 @@ int main(void) {
         batteryScaled = ADCvalue * (Vdd / 4095);
         batteryChargeStatus = ((batteryScaled - BAT_REF_LOW) / (BAT_REF_HI - BAT_REF_LOW)) * 100;
 
+        /* if (isTEC_off) {
+         *  if (fanTimer > 10 minutes) {
+         *      LATBbits.LATB15 = 0;
+         * }
+         * }
+         */
+
         if (counter < ARRAY_SIZE) {
             // add temp and battery readings to an array
             temps[counter] = actualTemp;
             battLevels[counter] = batteryChargeStatus;
             counter++;
             __delay_ms(100);
-        }
-        else {
+        } else {
             // average temperatures and bat. levels
             short i = 0;
             int tempSum = 0, battSum = 0;
-            for (;i < ARRAY_SIZE; i++) {
+            for (; i < ARRAY_SIZE; i++) {
                 tempSum += temps[i];
                 battSum += battLevels[i];
             }
             actualTemp = (tempSum / ARRAY_SIZE) + TEMP_CORRECTION;
             batteryChargeStatus = (battSum / ARRAY_SIZE) - BAT_CORRECTION;
             if (batteryChargeStatus < 0) batteryChargeStatus = 0;
+
+            /* if (actualTemp > userDesiredTemp) {
+             *  LATBbits.LATB14 = 1;
+             *  LATBbits.LATB15 = 1;
+             *  isTEC_off = false;
+             *  fanTimer = 0;
+             * }
+             * else {
+             *  LATBbits.LATB14 = 0;
+             *  isTEC_off = true;
+             *  fanTimer.start()
+             * }
+             */
             LCD_ClearCommand();
             displayData();
             counter = 0;
@@ -97,10 +116,10 @@ void initializeKeypadRowPins() {
      */
 
     // initially set keypad row pins to HIGH
-    LATBbits.LATB10 = 1;
+    LATBbits.LATB10 = 1; // ROW0
     LATBbits.LATB11 = 1;
     LATBbits.LATB12 = 1;
-    LATBbits.LATB13 = 1;
+    LATBbits.LATB13 = 1; // ROW3
 }
 
 void updateDisplayAfterKeypress(char keypress) {
@@ -151,7 +170,7 @@ bool updateOnesPlace(int keyValue) {
     bool isUpdateSuccessful = false;
     if ((userDesiredTemp / 10) == 3 && keyValue < 2) {
         errorMessage();
-    } else if ((userDesiredTemp / 10) == 4 && keyValue > 2) {
+    } else if ((userDesiredTemp / 10) == 4 && keyValue > 5) {
         errorMessage();
     } else {
         userDesiredTemp = (userDesiredTemp / 10) * 10; // e.g. 32 => 30
@@ -171,15 +190,15 @@ void displayData() {
     LCD_PrintString("%");
 
     LCD_SetDisplayAddressCommand(0x0040);
-    LCD_PrintString("DES:");
+    LCD_PrintString("TARGET:");
 
     LCD_PrintInteger(userDesiredTemp);
     LCD_PrintString("+F");
 
     if (isCursorInTensPlace) {
-        LCD_SetDisplayAddressCommand(0x0044);
+        LCD_SetDisplayAddressCommand(0x0047);
     } else {
-        LCD_SetDisplayAddressCommand(0x0045);
+        LCD_SetDisplayAddressCommand(0x0048);
     }
 }
 
